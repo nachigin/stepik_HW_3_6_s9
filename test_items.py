f185c50f7210254d299a8f7140644ec5939b473f
nachigin <58926409+nachigin@users.noreply.github.com>
2020-04-07 23:37:50 +0800

    исправлен селектор
    исправлен селектор

--- test_items.py
+++ test_items.py
@@ -6,5 +6,9 @@ def test_guest_should_see_basket_button(browser):
     browser.get(link)
     time.sleep(3)
 
-    but1 = browser.find_element_by_id("add_to_basket_form")
-    assert but1, "Кнопка отсутствует"
+    but1 = browser.find_elements_by_css_selector('button.btn-add-to-basket')
+    #but1.click()
+    time.sleep(3)
+    # browser.find_elements_by_css_selector('button.btn-add-to-basket')
+    # browser.find_element_by_id("add_to_basket_form")
+    assert but1, 'Кнопка отсутствует'
